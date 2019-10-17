27f477b60af92cf9c3a1bb5e70cd5697a9d1ce77
campcc <1210131782@qq.com>
2019-10-17 17:09:14 +0800

    feat: pick func completed

---
+++ src/index.js
@@ -0,0 +1,16 @@
+function pick(obj, properties) {
+	const isArray = Object.prototype.toString.call(properties) === '[object Array]'
+	const isString = typeof properties === 'string'
+	const isValid = isArray || isString
+	const formatProperties = isString ? [properties] : properties
+	const length = formatProperties.length
+	const result = {}
+	if (isValid) {
+		formatProperties.forEach(key => result[key] = obj[key])
+		return length ? result : {...obj}
+	} else {
+		throw new TypeError('properties expect an array or a string.')
+	}
+}
+
+export default pick
