077ec8e499a507ddf8859d9846d7e6fc8f51b781
StevenLee <32805629+campcc@users.noreply.github.com>
2019-10-17 18:07:20 +0800

    update pick style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function pick(obj, properties) {
-	const isArray = Object.prototype.toString.call(properties) === '[object Array]'
+  const isArray = Object.prototype.toString.call(properties) === '[object Array]'
 	const isString = typeof properties === 'string'
 	const isValid = isArray || isString
 	const formatProperties = isString ? [properties] : properties
